3a245d08fce317f78a2a5574eb22265afb7caee7
Ethan QUEHE-LE COZLER <ethan.quehelecozler@gmail.com>
2026-02-02 10:44:52 +0100

    Initial commit du projet C++

---
+++ client.cpp
@@ -0,0 +1,226 @@
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+
+
+
+int main() {
+    bool joue1 = false;
+    bool game = false;
+    bool bataille = false;
+    std::vector<std::string> carte;
+    std::string message_temp;
+    sf::TcpSocket socket;
+    std::string text_vict;
+    std::string text_j1;
+    std::string text_j2;
+    std::string plis_tempj1;
+    std::string plis_tempj2;
+    sf::IpAddress ip = "127.0.0.1"; // Adresse du serveur (local)
+
+    unsigned short port = 5325;
+
+    // 2. Connexion au serveur
+    std::cout << "Tentative de connexion au serveur..." << std::endl;
+    if (socket.connect(ip, port) != sf::Socket::Done) {
+        std::cerr << "Erreur : Impossible de rejoindre le serveur !" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Connecte !" << std::endl;
+
+    sf::Packet carte_recue;
+
+    if (socket.receive(carte_recue) == sf::Socket::Done) {
+        sf::Uint32 size;
+
+        // 1. On récupère d'abord la taille
+        if (carte_recue >> size) {
+
+
+            // 2. On boucle selon la taille reçue
+            for (sf::Uint32 i = 0; i < size; ++i) {
+                std::string nombre;
+                carte_recue >> nombre;
+                carte.push_back(nombre);
+            }
+
+
+        }
+
+    }
+
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Ca marche !");
+
+    sf::Texture fondImage,cartej1, cartej2;
+    if (!fondImage.loadFromFile("asset/plateau.jpeg")) {
+        return -1;
+    }
+    sf::Sprite Background,cardj1, cardj2;
+    Background.setTexture(fondImage);
+    float scaleX = 1920.f / fondImage.getSize().x;
+    float scaleY = 1080.f / fondImage.getSize().y;
+    Background.setScale(scaleX, scaleY);
+
+
+
+    cardj1.setPosition(600.f, 400.f);
+    cardj2.setPosition(1100.f, 400.f);
+    sf::Font font;
+
+    if (!font.loadFromFile("arial.ttf")) {
+        return -1;
+    }
+    sf::Text text;
+    sf::Text textj2;
+    sf::Text text3;
+    sf::Text plisj1;
+    sf::Text plisj2;
+
+
+    text.setPosition(150.f,300.f);
+    textj2.setPosition(650.f,300.f);
+    text3.setPosition(1200.f,100.f);
+    plisj1.setPosition(700.f,100.f);
+    plisj2.setPosition(700.f,150.f);
+
+    text.setFont(font);
+    textj2.setFont(font);
+    text3.setFont(font);
+    plisj1.setFont(font);
+    plisj2.setFont(font);
+
+    text.setFillColor(sf::Color::Red);
+    text3.setFillColor(sf::Color::Black);
+    plisj1.setFillColor(sf::Color::Black);
+    plisj2.setFillColor(sf::Color::Black);
+    socket.setBlocking(false);
+
+    while (window.isOpen()) {
+
+
+        sf::Event event;
+
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            if (!game && event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Space ) {
+                    if (joue1 == false) {
+                        text_j1 = carte.front();
+                        sf::Packet joue;
+                        joue << text_j1;
+                        if ( bataille ==  true) {
+                            if (cartej1.loadFromFile("asset/Cartes/00_G.png")) {
+                                cardj1.setTexture(cartej1);
+                                cardj1.setTextureRect(sf::IntRect(0, 0, cartej1.getSize().x, cartej1.getSize().y));
+                            }
+                            joue1=true;
+                        }
+                        else {
+                            if (cartej1.loadFromFile("asset/Cartes/" + carte.front() + ".png")) {
+                                cardj1.setTexture(cartej1);
+                                cardj1.setTextureRect(sf::IntRect(0, 0, cartej1.getSize().x, cartej1.getSize().y));
+                            }
+                            joue1 = true;
+                        }
+                        socket.send(joue);
+                        carte.erase(carte.begin());
+
+                    }
+                }
+
+
+
+            }
+        }
+        sf::Packet reponse;
+        if (socket.receive(reponse) == sf::Socket::Done) {
+            std::string recu;
+            reponse >> recu;
+            if (recu == "Bataille") {
+                joue1=false;
+                reponse >> bataille ;
+                text_vict = recu;
+                sf::Uint32 plis;
+                reponse >> plis;
+                plis_tempj2 = "le joueur adverse :" + std::to_string(plis);
+                plis_tempj1 = "Vous :" + std::to_string(carte.size());
+            }
+            else if (recu == "FINI" ) {
+                game = true;
+                std::string message;
+                reponse >> message;
+                text_vict =  "Tu as " +message+ "la partie";
+                text_j1 = "";
+                text_j2 = "";
+                sf::Uint32 plis;
+                reponse >> plis;
+                plis_tempj2 = "le joueur adverse :" + std::to_string(plis);
+                plis_tempj1 = "Vous :" + std::to_string(carte.size());
+            }
+            else if (recu == "Gagne" || recu == "Perdu") {
+                text_vict = "Tu as " + recu + " ce pli !";
+                joue1 = false;
+                if (recu == "Gagne") {
+                    int size;
+                    reponse >> size;
+                    for (int i = 0; i < size; ++i) {
+                        std::string element;
+                        reponse >> element;
+                        carte.push_back(element);
+                    }
+
+                }
+                sf::Uint32 plis;
+                reponse >> plis;
+                plis_tempj2 = "le joueur adverse :" + std::to_string(plis);
+                plis_tempj1 = "Vous :" + std::to_string(carte.size());
+                bataille = false;
+            }
+            else {
+                text_j2 = recu;
+                if (joue1 == true) {
+                    bataille= false;
+                }
+                if (cartej2.loadFromFile("asset/Cartes/" + text_j2  + ".png")) {
+                    cardj2.setTexture(cartej2);
+                    // On force la mise à jour du sprite au cas où la taille change
+                    cardj2.setTextureRect(sf::IntRect(0, 0, cartej2.getSize().x, cartej2.getSize().y));
+
+                }
+                joue1 = false;
+
+
+            }
+
+        }
+
+
+        plisj1.setString(plis_tempj1);
+        plisj2.setString(plis_tempj2);
+        text.setString(text_j1);
+        textj2.setString(text_j2);
+        text3.setString(text_vict);
+        window.clear(sf::Color::White);
+        window.draw(Background);
+        window.draw(cardj1);
+        window.draw(cardj2);
+        window.draw(text3);
+        window.draw(plisj1);
+        window.draw(plisj2);
+        window.display();
+
+    }
+
+
+
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,234 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <SFML/Network.hpp>
+
+int main() {
+    sf::TcpListener server;
+    std::vector<std::string> j1 ;
+    std::vector<std::string> j2 ;
+    bool bataille = false;
+    std::vector<int> plis;
+    std::vector<std::string> carte = {"01_S","02_S", "03_S","04_S", "05_S", "06_S","07_S","08_S","09_S","10_S","01_H","02_H", "03_H","04_H", "05_H", "06_H","07_H","08_H","09_H","10_H"};
+    //std::vector<std::string> carte = {"01_S","02_S","03_S","04_S","01_S","01_S","01_S","04_S"};
+    std::vector<sf::TcpSocket*> clients;
+    std::string joue_j1 = "";
+    std::string joue_j2 = "";
+    std::vector<std::string> temp_j1 ;
+    std::vector<std::string> temp_j2 ;
+    bool c = true;
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(carte.begin(), carte.end(), std::default_random_engine(seed));
+    for (std::string element : carte) {
+        if (c == false) {
+
+            j1.push_back(element);
+
+        }
+        else {
+            j2.push_back(element);
+
+        }
+        c=!c;
+    }
+    if (server.listen(5325) != sf::Socket::Done) {
+        std::cerr << "Error listening for connections" << std::endl;
+    }
+
+
+    while (clients.size() < 2) {
+        sf::TcpSocket* client = new sf::TcpSocket;
+        std::cout << "Tentative de connexion..." << std::endl;
+        if (server.accept(*client) == sf::Socket::Done) {
+            std::cout << "Joueur " << clients.size() + 1 << " connecte !" << std::endl;
+            clients.push_back(client);
+        } else {
+            delete client;
+        }
+    }
+
+
+    sf::Packet carte_j1;
+    sf::Packet carte_j2;
+    carte_j1 << static_cast<sf::Uint32>(j1.size());
+    carte_j2 << static_cast<sf::Uint32>(j2.size());
+    for (std::string element : j1) {
+        carte_j1 << element;
+    }
+    for (std::string element : j2) {
+        carte_j2 << element;
+    }
+
+
+    clients[0]->send(carte_j1);
+    clients[1]->send(carte_j2);
+
+
+    std::string cartej1="";
+    std::string cartej2="";
+    sf::SocketSelector joueur;
+    for (auto client : clients) {
+        joueur.add(*client);
+    }
+
+    while (true) {
+        if ((j1.size() == 0 && j2.size() == 10) || (j2.size() == 0 && j1.size() == 10)) {
+            sf::Packet victj1, victj2;
+            victj1 <<"FINI";
+            victj2 <<"FINI";
+            if (j1.size() == 0) {
+                victj2 <<"Gagne";
+                victj1 <<"Perdu";
+                victj1 << static_cast<sf::Uint32>(0);
+                victj2 << static_cast<sf::Uint32>(j1.size());
+
+            }
+            else {
+                victj1 <<"Gagne";
+                victj2 <<"Perdu";
+                victj1 << static_cast<sf::Uint32>(j2.size());
+                victj2 << static_cast<sf::Uint32>(0);
+            }
+            clients[0]->send(victj1);
+            clients[1]->send(victj2);
+
+        }
+        else if (bataille == true) {
+            if (joueur.wait(sf::milliseconds(100))) {
+                for (int i=0; i<2; ++i) {
+                    if (joueur.isReady(*clients[i])) {
+                        sf::Packet joue;
+                        if (clients[i]->receive(joue) == sf::Socket::Done) {
+                            std::string carte;
+                            joue >> carte;
+                            if (i == 0) {
+                                cartej1 = "00_G";
+
+                            }
+                            else {
+                                cartej2 = "00_G";
+
+                            }
+                            std::cout << "J" << i + 1 << " joue " << carte << std::endl;
+                            sf::Packet carte_autre;
+                            carte_autre << "00_G";
+                            clients[(i==0)?1:0]->send(carte_autre);
+
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            if (joueur.wait(sf::milliseconds(100))) {
+                for (int i=0; i<2; ++i) {
+                    if (joueur.isReady(*clients[i])) {
+                        sf::Packet joue;
+                        if (clients[i]->receive(joue) == sf::Socket::Done) {
+                            std::string carte;
+                            joue >> carte;
+                            if (i == 0) {
+                                cartej1 = carte;
+                                j1.erase(j1.begin());
+
+                            }
+                            else {
+                                cartej2 = carte;
+                                j2.erase(j2.begin());
+                            }
+                            std::cout << "J" << i + 1 << " joue " << carte << std::endl;
+                            sf::Packet carte_autre;
+                            carte_autre << carte;
+                            clients[(i==0)?1:0]->send(carte_autre);
+
+                        }
+                    }
+                }
+            }
+        }
+        if (cartej1 !="" && cartej2 != "") {
+                std::cout << cartej1 << cartej2 << std::endl;
+                sf::Packet victj1, victj2;
+                int v1 = std::stoi(cartej1.substr(0,2));
+                int v2 = std::stoi(cartej2.substr(0,2));
+                if (v1 == v2 && bataille == false ) {
+                    temp_j1.push_back(cartej1);
+                    temp_j2.push_back(cartej2);
+                    victj1 <<"Bataille" << true;
+                    victj2 <<"Bataille" << true;
+
+                    bataille =true;
+                }
+                else if (bataille == true) {
+                    temp_j1.push_back(j1.front());
+                    temp_j2.push_back(j2.front());
+                    j1.erase(j1.begin());
+                    j2.erase(j2.begin());
+                    bataille = false;
+                }
+
+                else if (v1 > v2) {
+                    if (temp_j1.size() != 0 && temp_j2.size() != 0) {
+                        int size = temp_j1.size() + temp_j2.size();
+                        temp_j1.push_back(cartej1);
+                        temp_j2.push_back(cartej2);
+                        victj1 << "Gagne" << size;
+                        victj2 << "Perdu";
+                        for (std::string element : temp_j1) {
+                            j1.push_back(element);
+                            victj1 << element;
+                        }
+                        for (std::string element : temp_j2) {
+                            j1.push_back(element);
+                            victj1 << element;
+                        }
+                        temp_j1.clear();
+                        temp_j2.clear();
+                    }
+                    else {
+                        int size = 2;
+                        victj1 << "Gagne" << size << cartej1 << cartej2;
+                        victj2 << "Perdu";
+                        j1.push_back(cartej1);
+                        j1.push_back(cartej2);
+                    }
+
+                }
+                else {
+                    if (temp_j1.size() != 0 && temp_j2.size() != 0) {
+                        int size = temp_j1.size() + temp_j2.size();
+                        temp_j1.push_back(cartej1);
+                        temp_j2.push_back(cartej2);
+                        victj1 << "Perdu";
+                        victj2 << "Gagne" << size;
+                        for (std::string element : temp_j1) {
+                            j2.push_back(element);
+                            victj2 << element;
+                        }
+                        for (std::string element : temp_j2) {
+                            j2.push_back(element);
+                            victj2 << element;
+                        }
+                        temp_j1.clear();
+                        temp_j2.clear();
+                    }
+                    else {
+                        int size = 2;
+                        victj1 << "Perdu";
+                        victj2 << "Gagne" << size << cartej1 << cartej2;
+                        j2.push_back(cartej2);
+                        j2.push_back(cartej1);
+                    }
+
+                }
+                victj1 << static_cast<sf::Uint32>(j2.size());
+                victj2 << static_cast<sf::Uint32>(j1.size());
+                clients[0]->send(victj1);
+                clients[1]->send(victj2);
+                cartej1 ="";
+                cartej2 ="";
+        }
+    }
+
+    return 0;
+}
